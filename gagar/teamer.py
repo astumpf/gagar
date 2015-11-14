e70a0a27de3145d27cef2dfd1d7e9780074bfab4
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2015-11-14 23:08:42 +0100

    tagar client reads config from file now

--- gagar/teamer.py
+++ gagar/teamer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.4
 import sys
+import configparser
 import socket
 from time import time, sleep
 import sched
@@ -13,10 +14,6 @@ from tagar.session import Session
 from tagar.player import Player
 from tagar.opcodes import *
 
-PORT = 55555
-TIMEOUT = 2
-UPDATE_RATE = 0.04
-
 
 class Teamer:
     def __init__(self, client):
@@ -26,14 +23,26 @@ class Teamer:
         self.last_world_buf = None
         self.session = None
 
+        config = configparser.ConfigParser({'address': 'localhost',
+                                            'port': '5000',
+                                            'password': '',
+                                            'update_rate': '0.1'})
+        config.read('tagar_client.cfg')
+
+        self.update_rate = config.getfloat('Settings', 'update_rate')
+
         self.scheduler = sched.scheduler(time, sleep)
-        self.scheduler.enter(UPDATE_RATE, 1, self.recv_update)
-        self.scheduler.enter(UPDATE_RATE, 1, self.send_update)
+        self.scheduler.enter(self.update_rate, 1, self.recv_update)
+        self.scheduler.enter(self.update_rate, 1, self.send_update)
         thread = threading.Thread(target=self.scheduler.run)
         thread.setDaemon(True)
         thread.start()
 
-        self.connect('127.0.0.1', 5555, "42")
+        address = config.get('Server', 'address')
+        port = config.getint('Server', 'port')
+        password = config.get('Server', 'password')
+
+        self.connect(address, port, password)
 
     def connect(self, addr, port, password):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -71,10 +80,9 @@ class Teamer:
                 sock.close()
                 return
 
-            id = buf.pop_null_str8()
+            print("Connected to tagar server: %s:%d" % (addr, port))
 
-            self.session = Session(id, sock)
-            print("Connected to team server with session id: ", id)
+            self.session = Session(buf.pop_null_str8(), sock)
 
         except socket.error:
             pass
@@ -85,21 +93,22 @@ class Teamer:
         self.session.disconnect()
 
     def recv_update(self):
-        self.scheduler.enter(UPDATE_RATE, 1, self.recv_update)
+        self.scheduler.enter(self.update_rate, 1, self.recv_update)
 
         if not self.session or not self.session.is_connected:
             return
 
-        msg = self.session.pop_msg()
-        if msg is None:
-            return
+        while 1:
+            msg = self.session.pop_msg()
+            if msg is None:
+                return
 
-        buf = BufferStruct(msg)
-        while len(buf.buffer) > 0:
-            self.dispatcher.dispatch(buf)
+            buf = BufferStruct(msg)
+            while len(buf.buffer) > 0:
+                self.dispatcher.dispatch(buf)
 
     def send_update(self):
-        self.scheduler.enter(UPDATE_RATE, 1, self.send_update)
+        self.scheduler.enter(self.update_rate, 1, self.send_update)
 
         if not self.session or not self.session.is_connected:
             return
