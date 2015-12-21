1b51f475cbc802536da40d93e95ebc10a51b1a58
Alexander Stumpf <stumpf@sim.tu-darmstadt.de>
2015-12-21 13:13:13 +0100

    refactored draw_skins

--- gagar/skins.py
+++ gagar/skins.py
@@ -38,25 +38,28 @@ skin_surface_cache = {}
 
 class CellSkins(Subscriber):
     @staticmethod
-    def on_draw_cells(c, w):
+    def draw(c, w, cell):
         c = c._cairo_context
+        name = cell.name.lower()
+        if name in special_names:
+            skin_data = get_skin(name)
+            if not skin_data:  # image is still being loaded
+                return  # TODO fancy loading circle animation
+            if name not in skin_surface_cache:
+                skin_surface_cache[name] = cairo.ImageSurface.create_from_png(io.BytesIO(skin_data))
+            skin_surface = skin_surface_cache[name]
+            skin_radius = skin_surface.get_width() / 2
+            c.save()
+            c.translate(*w.world_to_screen_pos(cell.pos))
+            scale = w.world_to_screen_size(cell.size / skin_radius)
+            c.scale(scale, scale)
+            c.translate(-skin_radius, -skin_radius)
+            c.set_source_surface(skin_surface, 0, 0)
+            c.new_sub_path()
+            c.arc(skin_radius, skin_radius, skin_radius, 0, TWOPI)
+            c.fill()
+            c.restore()
+
+    def on_draw_cells(self, c, w):
         for cell in w.world.cells.values():
-            name = cell.name.lower()
-            if name in special_names:
-                skin_data = get_skin(name)
-                if not skin_data:  # image is still being loaded
-                    continue  # TODO fancy loading circle animation
-                if name not in skin_surface_cache:
-                    skin_surface_cache[name] = cairo.ImageSurface.create_from_png(io.BytesIO(skin_data))
-                skin_surface = skin_surface_cache[name]
-                skin_radius = skin_surface.get_width() / 2
-                c.save()
-                c.translate(*w.world_to_screen_pos(cell.pos))
-                scale = w.world_to_screen_size(cell.size / skin_radius)
-                c.scale(scale, scale)
-                c.translate(-skin_radius, -skin_radius)
-                c.set_source_surface(skin_surface, 0, 0)
-                c.new_sub_path()
-                c.arc(skin_radius, skin_radius, skin_radius, 0, TWOPI)
-                c.fill()
-                c.restore()
+            self.draw(c, w, cell)
